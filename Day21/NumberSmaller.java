ad627e66e838bd1c535f30bc2f140382ab1ccea3
Souvik Raj Singh <souvikrajsingh@gmail.com>
2021-09-26 15:22:16 +0530

    Update Day21

--- Day21/NumberSmaller.java
+++ Day21/NumberSmaller.java
@@ -29,7 +29,7 @@ public class NumberSmaller {
 
         /*
         Shortened Method
-        
+
         for (int i = 0; i < nums.length; i++) {
             int count=0;
             for (int j = 0; j < nums.length; j++) {
