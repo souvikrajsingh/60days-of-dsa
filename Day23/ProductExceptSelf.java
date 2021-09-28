274ad5f9965a9d1222aea6f768b9822d067ae9e6
Souvik Raj Singh <souvikrajsingh@gmail.com>
2021-09-28 21:19:18 +0530

    Update Day23

--- Day23/ProductExceptSelf.java
+++ Day23/ProductExceptSelf.java
@@ -1,6 +1,7 @@
 package com.souvik.Day23;
 
 //https://leetcode.com/problems/product-of-array-except-self/
+//got Time Limit Exceeded error: Time complexity is too high
 
 import java.util.Arrays;
 
@@ -11,46 +12,32 @@ public class ProductExceptSelf {
         System.out.println(Arrays.toString(answer));
     }
     public static int[] productExceptSelf(int[] nums) {
+        int[] result = new int[nums.length];
+        for (int i = 0, tmp = 1; i < nums.length; i++) {
+            result[i] = tmp;
+            tmp *= nums[i];
+        }
+        for (int i = nums.length - 1, tmp = 1; i >= 0; i--) {
+            result[i] *= tmp;
+            tmp *= nums[i];
+        }
+        return result;
 
-        int [] answer =new int[nums.length];
-        int k=0,pro=1;
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
-                if(i==j)
-                {
-                    continue;
-                }
-                else
-                {
-                        pro=pro*nums[j];
-                        answer[k] = pro;
-
-                    }
-                }
-            pro = 1;
-            k = k + 1;
-            }
-        return answer;
         }
     }
+
     /*int [] answer =new int[nums.length];
-        int k=0,pro=1;
+    int pro=1;
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
-                if(i==j)
-                {
-                    continue;
-                }
-                else
-                {
-                        pro=pro*nums[j];
-                        answer[k] = pro;
+        for (int j = 0; j < nums.length; j++) {
+        if(i!=j)
+        {
+        pro=pro*nums[j];
+        answer[i] = pro;
 
-                    }
-                }
-            pro = 1;
-            k = k + 1;
-            }
+        }
+        }
+        pro = 1;
+        }
         return answer;*/
-
 
