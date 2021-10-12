5b57d7d02b112debd8a445f5523f5e35efd820ee
Souvik Raj Singh <souvikrajsingh@gmail.com>
2021-10-12 10:24:35 +0530

    Update Day31

--- Day31/LC_287_DuplicateNumber.java
+++ Day31/LC_287_DuplicateNumber.java
@@ -1,2 +1,25 @@
-package com.souvik.Day31;public class LC_287_DuplicateNumber {
+package com.souvik.Day31;
+
+//https://leetcode.com/problems/find-the-duplicate-number/
+
+public class LC_287_DuplicateNumber {
+
+    public static void main(String[] args)
+    {
+        int[] nums = { 1,3,3,4,5,2};
+        System.out.println(findDuplicate(nums));
+    }
+
+    public static int findDuplicate(int[] nums)
+    {
+        int i , j ;
+        for (i = 0; i < nums.length-1; i++) {
+            for (j = i +1 ; j < nums.length; j++) {
+                  if(nums [i] == nums [j]) {
+                    break;
+                }
+            }
+        }
+        return nums[i];
+    }
 }
