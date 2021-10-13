19a935b253cc5567fa7d234e4b4fdbcd584a529d
Souvik Raj Singh <souvikrajsingh@gmail.com>
2021-10-13 12:06:35 +0530

    Update Day32

--- Day32/pattern17.java
+++ Day32/pattern17.java
@@ -1,2 +1,25 @@
-package com.souvik.Day32;public class pattern17 {
+package com.souvik.Day32;
+
+public class pattern17 {
+    public static void main(String[] args) {
+
+        pattern17(4);
+    }
+    static void pattern17 ( int n)
+    {
+        for (int rows = 1; rows <= n; rows++) {
+
+            for (int spaces = 0; spaces < ( n - rows); spaces++) {
+                System.out.print(" ");
+            }
+            for (int cols = rows; cols >= 1 ; cols--) {
+
+                System.out.print(cols);
+            }
+            for (int cols = 2; cols <= rows ; cols++) {
+                System.out.print(cols);
+            }
+            System.out.println();
+        }
+    }
 }
