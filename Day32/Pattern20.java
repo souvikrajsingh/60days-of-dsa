0bd864174a4d47a437f24cd1a85601f548a5891e
Souvik Raj Singh <souvikrajsingh@gmail.com>
2021-10-13 13:19:51 +0530

    Update Day32

--- Day32/Pattern20.java
+++ Day32/Pattern20.java
@@ -3,7 +3,10 @@ package com.souvik.Day32;
 public class Pattern20 {
     public static void main(String[] args) {
 
-        pattern20(5);
+        /*pattern20(5);
+        pattern21(5);
+        pattern22(5);*/
+        pattern26(6);
     }
     static void pattern20( int n )
     {
@@ -26,4 +29,43 @@ public class Pattern20 {
             System.out.println();
         }
     }
+
+    static void pattern21( int n )
+    {
+        int c= 1;
+        for (int rows = 1; rows <= 5 ; rows++) {
+            for (int cols = 1; cols <= rows ; cols++) {
+                System.out.print(c + " ");
+                c=c+1;
+            }
+            System.out.println();
+
+        }
+    }
+    static void pattern22 (int n) {
+    }
+
+    static void pattern26(int n)
+    {
+        /*
+      1 1 1 1 1 1
+      2 2 2 2 2
+      3 3 3 3
+      4 4 4
+      5 5
+      6*/
+
+        int c= 1;
+        for (int rows = 1; rows <= n; rows++) {
+
+            for (int cols = 6; cols >= rows; cols --) {
+
+                System.out.print(c + " ");
+
+            }
+            c=c+1;
+            System.out.println();
+        }
+    }
+
 }
